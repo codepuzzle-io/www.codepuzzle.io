8949ecbd4f9381a0bcfbe8d65ae496970cd766d5
Laurent Abbal <laurent@abbal.com>
2023-11-04 11:10:05 +0900

    ~

--- resources/views/defi.blade.php
+++ resources/views/defi.blade.php
@@ -26,7 +26,7 @@ $asserts = '[' . trim($asserts, ',') . ']';
 	<div class="container-fluid">
 
 		@if(!$iframe)
-		<h1 class="mt-2 mb-5 text-center"><a class="navbar-brand m-1" href="{{ url('/') }}"><img src="{{ asset('img/codepuzzle.png') }}" height="20" alt="CODE PUZZLE" /></a></h1>
+		<h1 class="mt-2 mb-5 text-center"><a class="navbar-brand m-1" href="{{ url('/') }}"><img src="{{ asset('img/codepuzzle.png') }}" height="25" alt="CODE PUZZLE" /></a></h1>
 		@endif
 
 		@if ($defi->with_chrono == 1 OR $defi->with_nbverif == 1)
