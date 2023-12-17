31e68cbb37699810648053ec4dc52c18586ad7ae
Laurent Abbal <laurent@abbal.com>
2023-12-17 12:38:02 +0900

    ~

--- resources/views/inc-meta-puzzle.blade.php
+++ resources/views/inc-meta-puzzle.blade.php
@@ -34,6 +34,8 @@
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet">
 
 <!-- Styles -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
