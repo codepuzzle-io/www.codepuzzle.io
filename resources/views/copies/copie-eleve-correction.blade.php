b690f1499571424dd4f39567bdefdae7decd491c
Laurent Abbal <laurent@abbal.com>
2024-10-27 16:22:38 +0900

    v3

--- resources/views/copies/copie-eleve-correction.blade.php
+++ resources/views/copies/copie-eleve-correction.blade.php
@@ -110,7 +110,6 @@ if (isset($jeton_copie)) {
             }
         };
     </script>
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
     <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/python.min.js"></script>
