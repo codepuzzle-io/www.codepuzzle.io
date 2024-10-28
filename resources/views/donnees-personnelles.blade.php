3e1649ff06deea0aa96405eabdc26cfcb5ad2a90
Laurent Abbal <laurent@abbal.com>
2024-10-28 13:39:19 +0900

    v3

--- resources/views/donnees-personnelles.blade.php
+++ resources/views/donnees-personnelles.blade.php
@@ -1,17 +1,8 @@
-<?php
-include('github-import.php');
-$github_document = github_import('codepuzzle-io/www.codepuzzle.io/contents/donnees-personnelles.md');
-?>
-@include('inc-top')
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="fr">
 <head>
-	@php
-		$description = 'Politique de protection des données';
-		$description_og = '| Politique de protection des données';
-	@endphp
 	@include('inc-meta')
-    <title>{{ config('app.name') }} | Politique de protection des données</title>
+	<title>Code Puzzle | Données personnelles</title>
 </head>
 <body data-spy="scroll" data-target="#navbar-scrollspy" data-offset="20">
 
@@ -20,16 +11,107 @@ $github_document = github_import('codepuzzle-io/www.codepuzzle.io/contents/donne
 		<div class="row">
 			<div class="col-md-3">
 				<div class="sticky-top mb-5 pt-2">
-					@include('inc-nav')
-					<?php echo $github_document['menu'] ?>
+					<div class="container mt-4">
+
+						<div class="row">
+							<div class="col-md-6 text-left">
+								<a class="btn btn-light btn-sm mb-4" href="/" role="button"><i class="fas fa-arrow-left"></i></a>
+							</div>
+							<div class="col-md-6 text-right">
+							</div>
+						</div>
+					</div>
+					<div id="navbar-scrollspy" class="navbar pt-5">
+						<nav class="nav nav-pills flex-column">
+						<a class="nav-link" href="#item-2-1">INTRODUCTION</a>
+						<a class="nav-link" href="#item-2-2">DONNÉES TRAITÉES</a>
+						<a class="nav-link" href="#item-2-3">BASE LÉGALE</a>
+						<a class="nav-link" href="#item-2-4">UTILISATION</a>
+						<a class="nav-link" href="#item-2-5">HÉBERGEMENT DES DONNÉES À CARACTÈRE PERSONNEL</a>
+						<a class="nav-link" href="#item-2-6">DURÉE DE CONSERVATION</a>
+						<a class="nav-link" href="#item-2-7">FINALITÉS DES TRAITEMENTS</a>
+						<a class="nav-link" href="#item-2-8">COOKIES</a>
+						<a class="nav-link" href="#item-2-9">VOS DROITS SUR LES DONNÉES VOUS CONCERNANT</a>
+						<a class="nav-link" href="#item-2-10">DESTINATAIRES DES DONNÉES</a>
+						<a class="nav-link" href="#item-2-11">CARACTÈRE OBLIGATOIRE OU FACULTATIF DU RECUEIL DES DONNÉES</a>
+						<a class="nav-link" href="#item-2-12">EXERCER VOS DROITS</a>
+						</nav>
+					</div>
 				</div>
 			</div>
+
 			<div class="col-md-9 text-justify">
-				<?php echo $github_document['content'] ?>
+							
+				<div style="margin-top:30px;margin-bottom:1000px">
+
+					<h1 id="item-1-0">POLITIQUE DE PROTECTION DES DONNÉES</h1>
+					<p>Le RGPD impose une information complète et précise. Les modalités de fourniture et de présentation de cette information doivent être adaptées au contexte.</p>
+					<p>La transparence permet aux personnes concernées :</p>
+					<ul>
+					<li>de connaître la raison de la collecte des différentes données les concernant;</li>
+					<li>de comprendre le traitement qui sera fait de leurs données;</li>
+					<li>d'assurer la maîtrise de leurs données, en facilitant l'exercice de leurs droits.</li>
+					<li>Pour les responsables de traitement, elle contribue à un traitement loyal des données et permet d'instaurer une relation de confiance avec les personnes concernées.</li>
+					</ul>
+
+					<h2 id="item-2-1">INTRODUCTION</h2>
+					<p>Code Puzzle est amené à collecter et à traiter des informations dont certaines sont qualifiées de "données personnelles". Code Puzzle limite la collecte des données personnelles au strict nécessaire.</p>
+
+					<h2 id="item-2-2">DONNÉES TRAITÉES</h2>
+					<p>Les données personnelles collectées lors de la création d'un compte sont : prénom, nom, matière, établissement, adresse électronique et mot de passe. Aucun mot de passe n'est sauvegardé en clair. Tous les mots de passe sont chiffrés dans la base de données.</p>
+
+					<h2 id="item-2-3">BASE LÉGALE</h2>
+					<p>Les données personnelles ne sont collectées qu'après consentement obligatoire de l'utilisateur. Ce consentement est valablement recueilli lors de la création du compte (information et case à cocher). Le consentement est libre, clair et sans équivoque.</p>
+
+					<h2 id="item-2-4">UTILISATION</h2>
+					<p>Les données que vous nous transmettez sont utilisées :</p>
+					<ul>
+					<li>pour pouvoir accéder à la console;</li>
+					<li>dans le but de contacter la personne si nécessaire.</li>
+					</ul>
+					Ces données ne seront jamais cédées à un tiers, ni utilisées à d'autres fins que celles détaillées ci-dessus.
+
+					<h2 id="item-2-5">HÉBERGEMENT DES DONNÉES À CARACTÈRE PERSONNEL</h2>
+					<p>Les données à caractère personnel collectées et traitées sont hébergées en France sur le serveur de Code Puzzle. Les données à caractère personnel collectées par Code Puzzle ne sont transmises à aucun tier. Elles sont conservées sur le serveur de Code Puzzle jusqu'à expiration (voir "Durée de conservation").</p>
+
+					<h2 id="item-2-6">DURÉE DE CONSERVATION</h2>
+					<p>Les données des comptes sont conservées durant une durée maximale de 2 ans. Elles peuvent être supprimées à tout moment par l'utilisateur en cliquant, dans la console, sur "supprimer le compte".</p>
+
+					<h2 id="item-2-7">FINALITÉS DES TRAITEMENTS</h2>
+					<p>La collecte et le traitement des données répondent aux finalités suivantes : accéder à la console et communication.
+
+					<h2 id="item-2-8">COOKIES</h2>
+					Ce site n'utilise que deux 'cookies' nécessaires au fonctionnement du site. Ils permettent de gérer la navigation et d'éviter les attaques extérieures par injection de code malicieux via un formulaire. Ces 'cookies' ne contiennent aucune donnée personnelle et ils ont une durée de vie de trois heures.</p>
+
+					<h2 id="item-2-9">VOS DROITS SUR LES DONNÉES VOUS CONCERNANT</h2>
+					<p>Vous pouvez accéder et obtenir copie des données vous concernant, vous opposer au traitement de ces données, les faire rectifier ou les effacer depuis la console en cliquant sur « supprimer le compte ». Vous disposez également d'un droit à la limitation du traitement de vos données. Tout demande peut être faite en écrivant à l'adresse suivante : <a href="mailto:contact@codepuzzle.io">contact@codepuzzle.io</a></p>
+
+					<h2 id="item-2-10">DESTINATAIRES DES DONNÉES</h2>
+					<p>Seul le webmaster du site Code Puzzle a accès aux données. Vous pouvez le contacter à cette adresse : <a href="mailto:contact@codepuzzle.io">contact@codepuzzle.io</a>.</p>
+
+					<h2 id="item-2-11">CARACTÈRE OBLIGATOIRE OU FACULTATIF DU RECUEIL DES DONNÉES</h2>
+					<p>Aucun recueil de données personnelles n'est obligatoire.</p>
+
+					<h2 id="item-2-12">EXERCER VOS DROITS</h2>
+					<p>Pour toute information ou exercice de vos droits Informatique et Libertés sur les traitements de données personnelles gérés par la CNIL, vous pouvez contacter son délégué à la protection des données (DPO) :</p>
+					<ul>
+					<li>par ce <a href="https://www.cnil.fr/fr/webform/contactez-le-dpo-de-la-cnil" rel="nofollow">formulaire</a>
+					</li>
+					<li>ou par courrier (avec copie de votre pièce d'identité en cas d'exercice de vos droits) à l'adresse suivante:
+					Commission Nationale de l'Informatique et des Libertés
+					A l'attention du délégué à la protection des données (DPO)
+					3 Place de Fontenoy
+					TSA 80715
+					75334 PARIS CEDEX 07</li>
+					</ul>
+
+				</div>
 			</div>
-		</div>
+		</div><!-- /row -->
 
 	</div><!-- /container -->
+
 	@include('inc-bottom-js')
+
 </body>
 </html>
